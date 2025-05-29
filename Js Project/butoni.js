6f758203e3ff5df20a87bf3b3e2f24ba7bcf98e6
ardjanb1 <ardjanbardhoshi7@gmail.com>
2025-05-29 21:05:10 +0200

    Fshirë të gjitha skedarët

--- Js Project/butoni.js
+++
@@ -1,5 +0,0 @@
-const buton=document.getElementById('BUtoniNgjyra');
-buton.addEventListener('click', () => {
-    const ngjyrND=`#${Math.floor(Math.random()*16777215).toString(16)}`;
-    document.body.style.backgroundColor=ngjyrND;
-});
--- Js Project/script.js
+++
@@ -1,7 +0,0 @@
-const kat=document.getElementById('katrori');
-let hue=0;
-const update= () => { hue=(hue+1)%360;
-         kat.style.backgroundColor = `hsl(${hue},100%,50%)`;
-         requestAnimationFrame(update);
-};
-update();
